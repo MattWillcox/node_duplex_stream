a57ee2105c1003590cceeb139cd6cd5ab5923f4d
MattWillcox <mattjwillcox@gmail.com>
2018-03-21 20:18:04 -0700

    implemented custom transform and writeable classes

--- index.js
+++ index.js
@@ -1,59 +1,74 @@
-const { Duplex } = require('stream');
+const { Transform, Writable } = require('stream');
 const fs = require('fs');
 
+/*
+* @desc Read command line arguments
+* @arg[1] string - input file (default example.txt)
+* @arg[2] string - output file (default output.txt)
+*/
 const args = process.argv.slice(2);
 
 if(!args[0].endsWith('.txt')) args[0] += '.txt';
 if(!args[1].endsWith('.txt')) args[1] += '.txt';
+if(args[2]) throw new Error("too many args (max 2)")
 
 const inputFile = args[0] || 'example.txt';
 const outputFile = args[1] || 'output.txt';
 
-Duplex.Readable = fs.createReadStream(inputFile);
-Duplex.Writable = fs.createWriteStream(outputFile);
+inputData = fs.createReadStream(inputFile);
+outputData = fs.createWriteStream(outputFile);
 
-const data = {
+let startTime = Date.now();
+let endTime;
+
+// The output object shape from our stream transform
+const stats = {
+  totalTime: 0,
   totalLength: 0,
   totalLines: 0,
 };
 
-let startTime;
-let endTime;
-let throughput;
-
-Duplex.Readable.on('error', (error) => {
-  console.log("Error:", error);
+inputData.on('error', (error) => {
+  console.log(error);
 })
 
-Duplex.Readable.on('data', (chunk) => {
-  startTime = Date.now();
-  data.totalLength += chunk.length;
-  let dataString = chunk.toString();
-  let splitLines = dataString.split("\n");
-  data.totalLines += splitLines.length - 1;
+/* @desc Custom stream transform function
+* Reads input from file and outputs computed stats object
+*/
+const dataTransform = new Transform({
+  readableObjectMode: true,
+  transform(chunk, encoding, callback) {
+    stats.totalLength += chunk.length;
+    let linesArray = chunk.toString().split('\n');
+    stats.totalLines += linesArray.length - 1;
+    this.push(stats);
+    callback();
+  },
 });
 
-Duplex.Readable.on('end', function () {
-  Duplex.Readable.pipe(Duplex.Writable);
+const writer = new Writable({
+  objectMode: true,
+  write(chunk, encoding, callback) {
+    callback();
+  },
 });
 
-Duplex.Writable.on('error', function(error) {
-  console.log("Error:", error);
-})
+writer.on('error', (error) => {
+  console.log(error);
+});
 
-Duplex.Writable.on('pipe', (src) => {
+// Writes to our output file and prints final results (including throughput) to console
+writer.on('finish', () => {
   endTime = Date.now();
-  data.elapsedTime = (endTime - startTime) / 1000;
-  Duplex.Writable.write(JSON.stringify(data));
-  Duplex.Writable.end();
+  stats.totalTime = (endTime - startTime) / 1000;
+  const throughput = Math.round(stats.totalLength / stats.totalTime);
+  console.log(
+    `total time: ${stats.totalTime} secs \
+    total length: ${stats.totalLength} bytes \
+    total lines: ${stats.totalLines} \
+    throughput: ${throughput} bytes/sec`
+  );
+  outputData.write(JSON.stringify(stats));
 });
 
-Duplex.Writable.on('finish', () => {
-  throughput = data.totalLength / data.elapsedTime;
-  console.log(
-`Elapsed Time was ${data.elapsedTime} secs, \
-total Length was ${data.totalLength} bytes, \
-total Lines was ${data.totalLines}, \
-throughput was ${throughput} bytes/sec.`
-  );
-});
+inputData.pipe(dataTransform).pipe(writer);
